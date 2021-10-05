7430cb87eec2770dd154e33e37fbfb2942fc15ef
Frankelyn <frankelyn_garcia@ucne.edu.do>
2021-10-05 02:49:50 -0400

    Creando el contexto por segunda vez luego del error HTTP/2 en el commit

---
+++ DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P1_AP1_Frankelyn_20190276.Entidades;
+
+namespace P1_AP1_Frankelyn_20190276.DAL
+{
+    public class Contexto : DbContext
+    {
+        DbSet<Aportes> Aportes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= DATA\DbAportes.db");
+        }
+    }
+}
