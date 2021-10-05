69cd96bd547b0da2eaa14d2e3bf6161625bc680e
Frankelyn <frankelyn_garcia@ucne.edu.do>
2021-10-04 20:25:13 -0400

    Haciendo la primera migracion y creando la base de datos

--- obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace P1_AP1_Frankelyn_20190276 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/P1-AP1-Frankelyn-20190276;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/P1-AP1-Frankelyn-20190276;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
